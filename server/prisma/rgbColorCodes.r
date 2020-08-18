7106017d928097ea8408408d3423bb4edd3d3d20
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-08-18 15:49:49 +0900

    Correct the calculation of relative luminance #5
    We forgot dividing each RGB value with 255. We also forgot creating rgb(0, 0, 0) by failing to subtract 1 from ID

--- server/prisma/rgbColorCodes.r
+++ server/prisma/rgbColorCodes.r
@@ -3,20 +3,23 @@ library(tidyverse)
 nobs <- 256^3
 table_id_only <- tibble(id = 1:nobs)
 table_rgb_full <- mutate(table_id_only,
-       red = id%/%(256^2),
-       green = (id%%(256^2))%/%256,
-       blue = (id%%(256^2))%%256,
+       red = (id-1)%/%(256^2),
+       green = ((id-1)%%(256^2))%/%256,
+       blue = ((id-1)%%(256^2))%%256,
        # For the formula below, see https://www.w3.org/TR/WCAG20-TECHS/G17.html
-       s_red = ifelse(red > 10,
-                      ((red + 0.055)/1.055)^2.4,
-                      red / 12.92),
-       s_green = ifelse(green > 10,
-                        ((green + 0.055)/1.055)^2.4,
-                        green / 12.92),
-       s_blue = ifelse(blue > 10,
-                       ((blue + 0.055)/1.055)^2.4,
-                       blue / 12.92),
-       luminance = 0.2126*s_red + 0.7152*s_green + 0.0722*s_blue
+       red_srgb = red / 255,
+       green_srgb = green / 255,
+       blue_srgb = blue / 255,
+       r = ifelse(red_srgb > 0.03928,
+                      ((red_srgb + 0.055)/1.055)^2.4,
+                      red_srgb / 12.92),
+       g = ifelse(green_srgb > 0.03928,
+                        ((green_srgb + 0.055)/1.055)^2.4,
+                        green_srgb / 12.92),
+       b = ifelse(blue_srgb > 0.03928,
+                       ((blue_srgb + 0.055)/1.055)^2.4,
+                       blue_srgb / 12.92),
+       luminance = 0.2126*r + 0.7152*g + 0.0722*b
 )
 
 table_rgb <- select(table_rgb_full, id, red, green, blue, luminance)
