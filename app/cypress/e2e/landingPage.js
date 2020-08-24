f5ad57a007b880428578de6a32d1d5df30907ef8
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-08-24 16:54:20 +0900

    Refactor: use the helper function in the e2e test as well #8

--- app/cypress/e2e/landingPage.js
+++ app/cypress/e2e/landingPage.js
@@ -1,3 +1,5 @@
+import {getContrastRatio} from '../../utils/helpers';
+
 describe('Landing Page', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -7,19 +9,12 @@ describe('Landing Page', () => {
     cy.get('h1').should('have.text', 'Luminance Picker');
   });
 
-  it.only('shows the contrast ratio to pure black of the user-selected RGB color code', () => {
+  it('shows the contrast ratio to pure black of the user-selected RGB color code', () => {
     // setup
     const colorList = [
       {red: 123, green: 133, blue: 23},
       {red: 53, green: 2, blue: 223},
     ];
-    const contrastRatio = (r, g, b) => {
-      const normalize = eightBitNumber =>
-        Math.pow((eightBitNumber / 255 + 0.055) / 1.055, 2.4);
-      const relativeLuminance = (r, g, b) =>
-        0.2126 * normalize(r) + 0.7152 * normalize(g) + 0.0722 * normalize(b);
-      return ((relativeLuminance(r, g, b) + 0.05) / 0.05).toFixed(2);
-    };
     // execute
     colorList.forEach(color => {
       cy.findByLabelText(/r/i).clear().type(color.red.toString());
@@ -27,7 +22,7 @@ describe('Landing Page', () => {
       cy.findByLabelText(/b/i).clear().type(color.blue.toString());
       // verify
       cy.findByText(/contrast ratio with pure black/i).contains(
-        contrastRatio(color.red, color.green, color.blue),
+        getContrastRatio(color.red, color.green, color.blue),
       );
     });
   });
