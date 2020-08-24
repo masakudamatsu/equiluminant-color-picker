a8f70a341e7b9df9131d252e02e6bb1c82b82f49
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-08-24 12:19:22 +0900

    Revise the e2e test to allow multiple instances #8

--- app/cypress/e2e/landingPage.js
+++ app/cypress/e2e/landingPage.js
@@ -9,7 +9,10 @@ describe('Landing Page', () => {
 
   it.only('shows the contrast ratio to pure black of the user-selected RGB color code', () => {
     // setup
-    const colorList = [{red: 123, green: 133, blue: 23}];
+    const colorList = [
+      {red: 123, green: 133, blue: 23},
+      {red: 53, green: 2, blue: 223},
+    ];
     const contrastRatio = (r, g, b) => {
       const normalize = eightBitNumber =>
         Math.pow((eightBitNumber / 255 + 0.055) / 1.055, 2.4);
@@ -18,12 +21,14 @@ describe('Landing Page', () => {
       return ((relativeLuminance(r, g, b) + 0.05) / 0.05).toFixed(2);
     };
     // execute
-    cy.findByLabelText(/r/i).type(colorList[0].red.toString());
-    cy.findByLabelText(/g/i).type(colorList[0].green.toString());
-    cy.findByLabelText(/b/i).type(colorList[0].blue.toString()).blur();
-    // verify
-    cy.findByText(/contrast ratio with pure black/i).contains(
-      contrastRatio(colorList[0].red, colorList[0].green, colorList[0].blue),
-    );
+    colorList.forEach(color => {
+      cy.findByLabelText(/r/i).type(color.red.toString());
+      cy.findByLabelText(/g/i).type(color.green.toString());
+      cy.findByLabelText(/b/i).type(color.blue.toString()).blur();
+      // verify
+      cy.findByText(/contrast ratio with pure black/i).contains(
+        contrastRatio(color.red, color.green, color.blue),
+      );
+    });
   });
 });
