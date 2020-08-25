84db49d50f943eb1ffa65112f808002f2f350e63
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-08-25 18:14:09 +0900

    Show the contrast ratio on the /results page #18

--- app/pages/results.js
+++ app/pages/results.js
@@ -4,12 +4,14 @@ function Results(props) {
   return (
     <>
       <h1>Luminance Picker: Results</h1>
+      <p>{`Contrast ratio with pure black: ${props.contrastRatio}`}</p>
       <p>{`Selected hue range: ${props.hueRange.min}–${props.hueRange.max}`}</p>
     </>
   );
 }
 
 Results.propTypes = {
+  contrastRatio: PropTypes.string.isRequired,
   hueRange: PropTypes.object.isRequired,
 };
 
