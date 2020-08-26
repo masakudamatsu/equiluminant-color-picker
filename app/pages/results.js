cd80140492b2ec95f74d9405f5db9882bfa9f2f5
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-08-26 17:40:25 +0900

    Save GraphQL query as a JS variable with graphql-tag #20

--- app/pages/results.js
+++ app/pages/results.js
@@ -1,7 +1,19 @@
 import PropTypes from 'prop-types';
 import {withUrqlClient} from 'next-urql';
+import gql from 'graphql-tag';
 
 function Results(props) {
+  const FEED_QUERY = gql`
+    {
+      feed(hue: 270, contrastRatio: 5.21) {
+        red
+        green
+        blue
+        contrast_ratio
+        hue
+      }
+    }
+  `;
   return (
     <>
       <h1>Luminance Picker: Results</h1>
