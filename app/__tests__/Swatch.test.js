797519f77b7ea23947734ce071fd6b9076ed6177
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-08-26 18:02:44 +0900

    Revise the component test for Swatch #20
    Render with <ul> to avoid the accessibility warning. Update the snapshot. Use numbers as prop values. Should have been part of commit cce318f9b4a53487a8d4985bdf3b2f5e55577eff

--- app/__tests__/Swatch.test.js
+++ app/__tests__/Swatch.test.js
@@ -8,14 +8,16 @@ import 'jest-axe/extend-expect';
 import Swatch from '../components/Swatch';
 
 const colorList = [
-  {red: '123', green: '133', blue: '23'},
-  {red: '53', green: '2', blue: '223'},
+  {red: 123, green: 133, blue: 23},
+  {red: 53, green: 2, blue: 223},
 ];
 
 test('shows the color as specified in props', () => {
   colorList.forEach(color => {
     const {container, getByText} = render(
-      <Swatch r={color.red} g={color.green} b={color.blue} />,
+      <ul>
+        <Swatch r={color.red} g={color.green} b={color.blue} />
+      </ul>,
     );
     expect(
       getByText(`rgb(${color.red}, ${color.green}, ${color.blue})`),
@@ -27,30 +29,36 @@ test('shows the color as specified in props', () => {
 
 test('renders correctly', () => {
   const {container} = render(
-    <Swatch
-      r={colorList[0].red}
-      g={colorList[0].green}
-      b={colorList[0].blue}
-    />,
+    <ul>
+      <Swatch
+        r={colorList[0].red}
+        g={colorList[0].green}
+        b={colorList[0].blue}
+      />
+    </ul>,
   );
   expect(container).toMatchInlineSnapshot(`
     <div>
-      <div
-        style="background-color: rgb(123, 133, 23); padding-top: 30%; width: 30%;"
-      >
-        rgb(123, 133, 23)
-      </div>
+      <ul>
+        <li
+          style="background-color: rgb(123, 133, 23); margin-top: 1px; padding-top: 30%; width: 30%;"
+        >
+          rgb(123, 133, 23)
+        </li>
+      </ul>
     </div>
   `);
 });
 
 test('is accessible', async () => {
   const {container} = render(
-    <Swatch
-      r={colorList[0].red}
-      g={colorList[0].green}
-      b={colorList[0].blue}
-    />,
+    <ul>
+      <Swatch
+        r={colorList[0].red}
+        g={colorList[0].green}
+        b={colorList[0].blue}
+      />
+    </ul>,
   );
   const results = await axe(container);
   expect(results).toHaveNoViolations();
