9f39fa5d157d14716c945c75daaaa7de3aac3c3b
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-09-14 19:25:18 +0900

    Refactor the size parameters in rem by default #37 #47
    When we need px, then we multiply with oneRemPx (=16)

--- app/theme/font.js
+++ app/theme/font.js
@@ -1,13 +1,16 @@
+// If no unit is mentioned in the variable name, the unit is rem.
+// Otherwise the unit is explicitly mentioned in the variable name (e.g. xHeightPx).
+
+// # of px for 1rem
+const oneRemPx = 16;
+
 // Set x-height and modular scale
-const xHeightPx = {
-  mobile: 18 * (10 / 21), // Medium.com
-  desktop: 10, // Medium.com and Dev.to
+const baseXheight = {
+  mobile: (18 * (10 / 21)) / oneRemPx, // Medium.com
+  desktop: 10 / oneRemPx, // Medium.com and Dev.to
 };
 
 const modularScale = 2;
-
-// # of pixels for 1rem
-const oneRemPx = 16;
 
 // Font metrics
 const sfProText = {
@@ -19,9 +22,10 @@ const sfProText = {
 }; // obtained from opentype.js
 
 // Convert x-height into font-size
-const getFontSizeRemFromXHeightPx = (xHeightPx, scale, fontMetrics) => {
-  const xHeightRem = (xHeightPx * Math.pow(modularScale, scale)) / oneRemPx;
-  return xHeightRem * (fontMetrics.unitsPerEm / fontMetrics.xHeight);
+const getFontSizeFromXheight = (baseXheight, scale, fontMetrics) => {
+  const xHeight = baseXheight * Math.pow(modularScale, scale);
+  const xHeightToFontSizeRatio = fontMetrics.xHeight / fontMetrics.unitsPerEm;
+  return xHeight / xHeightToFontSizeRatio;
 };
 
 const font = {
@@ -29,13 +33,11 @@ const font = {
     family:
       "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif",
     size: {
-      mobile: getFontSizeRemFromXHeightPx(
-        xHeightPx.mobile,
-        0,
-        sfProText,
-      ).toFixed(4),
-      desktop: getFontSizeRemFromXHeightPx(
-        xHeightPx.desktop,
+      mobile: getFontSizeFromXheight(baseXheight.mobile, 0, sfProText).toFixed(
+        4,
+      ),
+      desktop: getFontSizeFromXheight(
+        baseXheight.desktop,
         0,
         sfProText,
       ).toFixed(4),
@@ -43,7 +45,7 @@ const font = {
   },
   sideMarginPx: {
     // in pixel, because we do not want it to be enlarged when the user increases the font size.
-    mobile: xHeightPx.mobile * Math.pow(modularScale, 1),
+    mobile: baseXheight.mobile * Math.pow(modularScale, 1) * oneRemPx,
   },
 };
 
