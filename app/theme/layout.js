e10311a363a51a0fc6a6fdb13c136245459e3265
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-09-15 16:44:36 +0900

    Style the color code text field #39

---
+++ app/theme/layout.js
@@ -0,0 +1,159 @@
+// If no unit is mentioned in the variable name, the unit is rem.
+// Otherwise the unit is explicitly mentioned in the variable name (e.g. xHeightPx).
+
+// # of px for 1rem
+const oneRemPx = 16;
+
+// Set x-height and modular scale
+const baseXheight = {
+  mobile: (18 * (10 / 21)) / oneRemPx, // Medium.com
+  desktop: 10 / oneRemPx, // Medium.com and Dev.to
+};
+
+const modularScale = 2;
+
+// Font metrics
+const sfProText = {
+  unitsPerEm: 2048,
+  xHeight: 1078,
+  capHeight: 1443,
+  ascender: 1950,
+  descender: 494,
+}; // obtained from opentype.js
+
+// const Roboto = {
+//   unitsPerEm: 1000, // 2048,
+//   xHeight: 530, // 1082,
+//   capHeight: 715, // 1456,
+//   ascender: 930, // 1900,
+//   descender: 240, // 500,
+// }; // Measured by myself, by setting font-size: 100px
+
+const Roboto = {
+  unitsPerEm: 2048,
+  xHeight: 1082,
+  capHeight: 1456,
+  ascender: 1900,
+  descender: 500,
+}; // Measured by myself, by setting font-size: 100px
+
+// Convert x-height into font-size
+const getFontSizeFromXheight = (baseXheight, scale, fontMetrics) => {
+  const xHeight = baseXheight * Math.pow(modularScale, scale);
+  const xHeightToFontSizeRatio = fontMetrics.xHeight / fontMetrics.unitsPerEm;
+  return xHeight / xHeightToFontSizeRatio;
+};
+
+// Convert cap-height into font-size
+const getFontSizeFromCapHeight = (baseCapHeight, scale, fontMetrics) => {
+  const capHeight = baseCapHeight * Math.pow(modularScale, scale);
+  const capHeightToFontSizeRatio =
+    fontMetrics.capHeight / fontMetrics.unitsPerEm;
+  return capHeight / capHeightToFontSizeRatio;
+};
+
+// Font size
+const fontSize = {
+  bodyText: {
+    mobile: getFontSizeFromXheight(baseXheight.mobile, 0, Roboto),
+  },
+  input: {
+    mobile: getFontSizeFromCapHeight(baseXheight.mobile, 1, Roboto),
+  },
+  label: {
+    mobile: getFontSizeFromCapHeight(baseXheight.mobile, 0, Roboto),
+  },
+};
+
+const lineHeight = {
+  // in rem
+  bodyText: {
+    mobile: baseXheight.mobile * (1 + modularScale),
+    desktop: baseXheight.desktop * (1 + modularScale),
+  },
+};
+
+const lineHeightEm = {
+  bodyText:
+    lineHeight.bodyText.desktop /
+    getFontSizeFromXheight(baseXheight.desktop, 0, Roboto),
+};
+
+const getTextCropBottom = (fontMetrics, lineHeightEm) => {
+  return (
+    fontMetrics.descender / fontMetrics.unitsPerEm + (lineHeightEm - 1) / 2
+  );
+};
+
+const getTextCropTopX = (fontMetrics, lineHeightEm) => {
+  return (
+    (fontMetrics.ascender - fontMetrics.xHeight) / fontMetrics.unitsPerEm +
+    (lineHeightEm - 1) / 2
+  );
+};
+
+const getTextCropTopCap = (fontMetrics, lineHeightEm) => {
+  return (
+    (fontMetrics.ascender - fontMetrics.capHeight) / fontMetrics.unitsPerEm +
+    (lineHeightEm - 1) / 2
+  );
+};
+
+const layout = {
+  body: {
+    fontSize: {
+      mobile: fontSize.bodyText.mobile.toFixed(4),
+    },
+    lineHeight: lineHeightEm.bodyText,
+  },
+  colorCodeField: {
+    paddingPx: {
+      mobile: (baseXheight.mobile * oneRemPx).toFixed(4),
+    },
+  },
+  input: {
+    borderWidthPx: 2,
+    fontSize: {
+      mobile: fontSize.input.mobile.toFixed(4),
+    },
+    paddingBottomPx: {
+      mobile:
+        (baseXheight.mobile * Math.pow(modularScale, 1) -
+          getTextCropBottom(Roboto, 1) * fontSize.input.mobile) *
+        oneRemPx,
+    },
+    paddingTopPx: {
+      // Input field's text box does not include line-height, it seems.
+      mobile:
+        (baseXheight.mobile * (2 + Math.pow(modularScale, 1)) -
+          getTextCropTopCap(Roboto, 1) * fontSize.input.mobile) *
+        oneRemPx,
+    },
+  },
+  label: {
+    fontSize: {
+      mobile: fontSize.label.mobile.toFixed(4),
+    },
+    verticalSpacePx: {
+      // in pixel, because we do not want it to be enlarged when the user increases the font size.
+      mobile: baseXheight.mobile * oneRemPx,
+    },
+    horizontalSpacePx: {
+      // in pixel, because we do not want it to be enlarged when the user increases the font size.
+      mobile: baseXheight.mobile * oneRemPx,
+    },
+  },
+  sideMarginPx: {
+    // in pixel, because we do not want it to be enlarged when the user increases the font size.
+    mobile: baseXheight.mobile * Math.pow(modularScale, 1) * oneRemPx,
+  },
+  textCrop: {
+    bodyText: {
+      bottom: getTextCropBottom(Roboto, lineHeightEm.bodyText).toFixed(4),
+      topCap: getTextCropTopCap(Roboto, lineHeightEm.bodyText).toFixed(4),
+      topX: getTextCropTopX(Roboto, lineHeightEm.bodyText).toFixed(4),
+    },
+  },
+};
+
+export default layout;
--- app/theme/style.js
+++ app/theme/style.js
@@ -1,6 +1,42 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import color from './color';
-import font from './font';
+import layout from './layout';
+
+// Text box cropping
+const textCrop = {
+  bodyText: {
+    capHeight: css`
+      &::before,
+      &::after {
+        content: '';
+        display: block;
+        height: 0;
+        width: 0;
+      }
+      &::before {
+        margin-bottom: -${layout.textCrop.bodyText.topCap}em;
+      }
+      &::after {
+        margin-top: -${layout.textCrop.bodyText.bottom}em;
+      }
+    `,
+    xHeight: css`
+      &::before,
+      &::after {
+        content: '';
+        display: block;
+        height: 0;
+        width: 0;
+      }
+      &::before {
+        margin-bottom: -${layout.textCrop.bodyText.topX}em;
+      }
+      &::after {
+        margin-top: -${layout.textCrop.bodyText.bottom}em;
+      }
+    `,
+  },
+};
 
 // HTML elements
 export const Abbr = styled.abbr`
@@ -16,29 +52,39 @@ export const Input = styled.input.attrs(props => ({
   autoComplete: 'off', // to remove Webkit browser's default style that cannot be overriden. See https://stackoverflow.com/questions/2338102/override-browser-form-filling-and-input-highlighting-with-html-css
 }))`
   background-color: inherit;
-  border: 2px solid white;
-  border-color: ${props => (props.error ? 'red' : 'white')};
+  border: ${layout.input.borderWidthPx.toFixed()}px solid white;
+  border-color: ${props => (props.error ? 'red' : 'grey')};
   border-radius: 4px;
-  color: inherit;
-  font-size: 80px; /* x-height x 2^3 */
+  font-size: ${layout.input.fontSize.mobile}rem;
   height: 100%;
-  line-height: 1;
-  padding: 16px;
+  padding: ${layout.input.paddingTopPx.mobile.toFixed(4)}px
+    ${layout.label.horizontalSpacePx.mobile.toFixed(4)}px
+    ${layout.input.paddingBottomPx.mobile.toFixed(4)}px;
   text-align: center;
   width: 100%;
 
   &:active,
   &:hover,
   &:focus {
-    border-width: 3px;
+    border-color: white;
     outline: none;
   }
 `;
 
 export const Label = styled.label`
+  font-size: ${layout.label.fontSize.mobile}rem;
   position: absolute;
-  top: 8px;
-  left: 16px;
+  top: ${(
+    layout.label.verticalSpacePx.mobile +
+    layout.input.borderWidthPx +
+    2
+  ).toFixed(
+    4,
+  )}px; /* For some reason, the label element will be placed 2px above the edge of its wrapper div. We fix this by adding 2px. */
+  left: ${(
+    layout.label.horizontalSpacePx.mobile + layout.input.borderWidthPx
+  ).toFixed(4)}px;
+  ${textCrop.bodyText.capHeight}
 `;
 
 export const ParagraphErrorMessage = styled.p`
@@ -49,19 +95,16 @@ export const ParagraphErrorMessage = styled.p`
 export const ColorCodeField = styled.div`
   background-color: ${props => props.backgroundColor};
   color: ${props => (props.darkMode ? color.font : color.darkMode.font)};
-  padding: ${font.sideMarginPx.mobile}px;
+  padding: ${layout.colorCodeField.paddingPx.mobile}px;
   width: 100%;
 `;
 
 export const InputWrapper = styled.div`
   background-color: inherit;
-  color: white;
-  font-family: system-ui;
-  line-height: 1;
   position: relative;
-  width: 80%;
+  width: 100%;
 `;
 
 export const SideMarginSetter = styled.div`
-  margin: ${font.sideMarginPx.mobile}px;
+  margin: ${layout.sideMarginPx.mobile}px;
 `;
