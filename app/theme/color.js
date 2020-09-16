d3df2c36b61b2324ce61100596bcc67435d12767
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-09-16 16:26:47 +0900

    Refactor the code for the color palette #39 #40
    To make explicit what each color means

--- app/theme/color.js
+++ app/theme/color.js
@@ -1,11 +1,20 @@
+// Color palette
+
+// 1:4.5 contrast ratio to threshold rgb(117 ,117. 117)
+const blackAAA = 'rgb(2, 2, 2)';
+// 1:3 contrast ratio to threshold rgb(117 ,117. 117)
+const blackAA = 'rgb(44, 44, 44)';
+// 3:1 contrast ratio to threshold rgb(117 ,117. 117)
+const whiteAA = 'rgb(209, 209, 209)';
+// 4.5:1 contrast ratio to threshold rgb(117 ,117. 117)
+const whiteAAA = 'rgb(253, 253, 253)';
+
 const color = {
-  background:
-    'rgb(209, 209, 209)' /* 3:1 contrast ratio to threshold rgb(117,117,117) */,
-  font: 'rgb(44, 44, 44)',
+  background: whiteAA,
+  font: blackAA,
   darkMode: {
-    background:
-      'rgb(44, 44, 44)' /* 1:3 contrast ratio to threshold rgb(117,117,117) */,
-    font: 'rgb(209, 209, 209)',
+    background: blackAA,
+    font: whiteAA,
   },
 };
 
