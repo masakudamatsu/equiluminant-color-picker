3e2d03fc33f02e5dc3ceb00ba50d8555ad7b5ef1
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-09-22 14:22:43 +0900

    Fix the contrast ratio calculation #8

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,9 +1,15 @@
 export const getContrastRatio = (r, g, b) => {
-  const normalize = eightBitNumber =>
-    Math.pow((eightBitNumber / 255 + 0.055) / 1.055, 2.4);
+  const normalize = eightBitNumber => {
+    if (eightBitNumber / 255 <= 0.03928) {
+      return eightBitNumber / 255 / 12.92;
+    } else {
+      return Math.pow((eightBitNumber / 255 + 0.055) / 1.055, 2.4);
+    }
+  };
   const relativeLuminance = (r, g, b) =>
     0.2126 * normalize(r) + 0.7152 * normalize(g) + 0.0722 * normalize(b);
   return ((relativeLuminance(r, g, b) + 0.05) / 0.05).toFixed(2);
+  // See https://www.w3.org/TR/WCAG20-TECHS/G17.html
 };
 
 export const getRgbFromHex = hexCode => {
