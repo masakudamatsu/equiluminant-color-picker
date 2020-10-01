9910584ca6e55563851b3d5b7abb30c57b26ee44
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-10-01 14:12:13 +0900

    Remove the app folder #87
    to prepare a bug report with the "minimum" complete reproducible code

--- app/components/ColorCodeDisplay.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-function ColorCodeDisplay(props) {
-  return <p id="color-code-display">{props.children}</p>;
-}
-
-ColorCodeDisplay.propTypes = {};
-
-export default ColorCodeDisplay;
--- app/theme/font.js
+++
@@ -1,9 +0,0 @@
-const font = {
-  body: {
-    family:
-      "Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Helvetica Neue', Arial, sans-serif",
-    weight: 400,
-  },
-};
-
-export default font;
