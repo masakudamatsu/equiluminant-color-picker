47ee8cd85c759bdf481790cfd66fbdfdce27b152
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-10-12 10:26:02 +0900

    #98 Reset the browser default style of range input

--- app/theme/GlobalStyle.js
+++ app/theme/GlobalStyle.js
@@ -322,6 +322,35 @@ input {
   color: inherit; /* Prevent Chrome from applying "internal-light-dark" to override the body element's color property */
 }
 
+/* Reset range input style */
+/* See https://css-tricks.com/styling-cross-browser-compatible-range-inputs-css/#applying-base-css-styles */
+input[type=range] {
+  /* Hides the slider so that custom slider can be made */
+  -webkit-appearance: none;
+  /* Specific width is required for Firefox. */
+  width: 100%;
+  /* Remove white background in Chrome */
+  background: transparent;
+}
+
+input[type=range]::-webkit-slider-thumb {
+  -webkit-appearance: none;
+}
+
+input[type=range]:focus {
+  /* Removes the blue border. You should probably do some kind of focus styling for accessibility reasons though. */
+  outline: none;
+}
+
+input[type=range]::-ms-track {
+  width: 100%;
+  cursor: pointer;
+  /* Hides the slider so custom styles can be added */
+  background: transparent; 
+  border-color: transparent;
+  color: transparent;
+}
+
 `;
 
 export default GlobalStyle;
