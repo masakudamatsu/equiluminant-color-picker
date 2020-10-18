702590f4f1dc419e3239e5db2329e431260fe21a
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-10-18 11:55:43 +0900

    #75 Revise the template component test script
    So Jest will skip to run this file

--- app/__tests__/components/Template.test.js
+++ app/__tests__/components/Template.test.js
@@ -1,27 +1,38 @@
-// import React from 'react';
-// import {cleanup, fireEvent, render} from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import '@testing-library/jest-dom/extend-expect';
-// import {axe} from 'jest-axe';
-// import 'jest-axe/extend-expect';
+import React from 'react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
+import {axe} from 'jest-axe';
+import 'jest-axe/extend-expect';
+
+// How to use this template
 //
-// import Template from '../../components/Template';
+// Step 1
+// Replace Template with the name of a component
 //
-// afterEach(() => {
-//   jest.clearAllMocks();
-// });
+// Step 2
+// Delete lines 19 and 38 (the describe() function), which makes Jest skip to run this test file
 //
-// let container, getByLabelText;
-// beforeEach(() => {
-//   return ({container, getByLabelText} = render(<Template />));
-// });
-//
-// test('renders correctly', () => {
-//   expect(container).toMatchInlineSnapshot();
-// });
-//
-// test('is accessible', async () => {
-//   const results = await axe(container);
-//   expect(results).toHaveNoViolations();
-//   cleanup();
-// });
+
+import Template from '../../components/Template';
+
+describe.skip('Delete this line', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  let container, getByLabelText;
+  beforeEach(() => {
+    return ({container, getByLabelText} = render(<Template />));
+  });
+
+  test('renders correctly', () => {
+    expect(container).toMatchInlineSnapshot();
+  });
+
+  test('is accessible', async () => {
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+    cleanup();
+  });
+});
