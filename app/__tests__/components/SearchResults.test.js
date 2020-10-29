7a91340e09fbef0a4836eb7fde77f29be5ab5ee4
Masa Kudamatsu <masakudamatsu@gmail.com>
2020-10-29 19:41:39 +0900

    #114 Write default tests for SearchResults component

---
+++ app/__tests__/components/SearchResults.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
+import {axe} from 'jest-axe';
+import 'jest-axe/extend-expect';
+
+import SearchResults from '../../components/SearchResults';
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+let container, getByLabelText;
+beforeEach(() => {
+  return ({container, getByLabelText} = render(<SearchResults />));
+});
+
+test('renders correctly', () => {
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <div />
+    </div>
+  `);
+});
+
+test('is accessible', async () => {
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+  cleanup();
+});
